772aa5d718d4d5a26e59aed60abc5a9e284ee1a1
Divy Patel <125264776+Divy0409@users.noreply.github.com>
2026-02-27 16:58:39 -0500

    minor update

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -1,4 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default {
-  base: "/",
-};
+export default defineConfig({
+  plugins: [react()],
+  base: '/',   // IMPORTANT for username.github.io repo
+})
